4ee1f2373208d5e330c8a42d73101edd60936d54
vaaast-lake <kown9512@gmail.com>
2025-11-06 19:53:59 +0900

    feat: fs repository 구현

---
+++ backend/src/file-system/file-system.repository.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { DataSource, EntityManager, Repository } from 'typeorm';
+import { FileSystem } from './entities/file-system.entity';
+
+@Injectable()
+export class FileSystemRepository {
+  private repo: Repository<FileSystem>;
+
+  constructor(private readonly dataSource: DataSource) {
+    this.repo = this.dataSource.getRepository(FileSystem);
+  }
+
+  withManager(manager: EntityManager): FileSystemRepository {
+    const newRepo = new FileSystemRepository(this.dataSource);
+    newRepo.repo = manager.getRepository(FileSystem);
+    return newRepo;
+  }
+
+  async getRootDirectory(): Promise<FileSystem | null> {
+    return this.repo.findOne({ where: { path: '/' } });
+  }
+
+  async findByPath(path: string): Promise<FileSystem | null> {
+    return this.repo.findOne({ where: { path } });
+  }
+
+  async save(fileSystem: FileSystem): Promise<FileSystem> {
+    return this.repo.save(fileSystem);
+  }
+}
