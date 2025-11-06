c73fb8e39776dd9b6542b9d6b2ed68e7728dea98
vaaast-lake <kown9512@gmail.com>
2025-11-06 20:34:40 +0900

    feat: global pipe 설정 추가

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 없는 속성 제거
+      forbidNonWhitelisted: true, // DTO에 없는 속성 있으면 에러
+      transform: true, // 타입 자동 변환
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
