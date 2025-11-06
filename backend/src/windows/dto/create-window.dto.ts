5f86c9949ddb205b39f251ec88d171ca1e3822c4
vaaast-lake <kown9512@gmail.com>
2025-11-06 17:14:55 +0900

    feat: window 생성 Dto 추가

--- backend/src/windows/dto/create-window.dto.ts
+++ backend/src/windows/dto/create-window.dto.ts
@@ -1,1 +1,33 @@
-export class CreateWindowDto {}
+import {
+  IsString,
+  IsInt,
+  IsEnum,
+  IsOptional,
+  Min,
+  MaxLength,
+} from 'class-validator';
+import { WindowType } from '../types';
+
+export class CreateWindowDto {
+  @IsString()
+  @MaxLength(255)
+  title: string;
+
+  @IsInt()
+  x: number;
+
+  @IsInt()
+  y: number;
+
+  @IsInt()
+  @Min(100)
+  width: number;
+
+  @IsInt()
+  @Min(100)
+  height: number;
+
+  @IsEnum(WindowType)
+  @IsOptional()
+  windowType?: WindowType = WindowType.TERMINAL;
+}
