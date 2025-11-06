a32c8676672234cf378b27d7cdeba6847f3c9447
vaaast-lake <kown9512@gmail.com>
2025-11-06 14:33:21 +0900

    feat: sessions 모듈 초기화

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WindowsModule } from './windows/windows.module';
 import { FileSystemModule } from './file-system/file-system.module';
+import { SessionsModule } from './sessions/sessions.module';
 
 @Module({
-  imports: [WindowsModule, FileSystemModule],
+  imports: [WindowsModule, FileSystemModule, SessionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
