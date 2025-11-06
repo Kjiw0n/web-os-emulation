b2217e7b08a9dc0db646f184a3b2c7b660d111f9
Kjiw0n <rwd0904@naver.com>
2025-11-06 18:38:23 +0900

    chore: svgr 설치 및 아이콘 세팅

---
+++ frontend/src/assets/Icons/index.ts
@@ -0,0 +1,17 @@
+import DarkMode from "./DarkMode.svg";
+import LightMode from "./LightMode.svg";
+import Power from "./Power.svg";
+import PowerBig from "./PowerBig.svg";
+import Restart from "./Restart.svg";
+import Terminal from "./Terminal.svg";
+
+const Icn = {
+  DarkMode,
+  LightMode,
+  Power,
+  PowerBig,
+  Restart,
+  Terminal,
+};
+
+export default Icn;
