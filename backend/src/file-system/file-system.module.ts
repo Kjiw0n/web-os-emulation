d0117441eff9c90feba5e45786d3d43d3ac9d987
vaaast-lake <kown9512@gmail.com>
2025-11-06 15:53:05 +0900

    feat: file-system, sessions, windows 모듈에 typeorm 추가

--- backend/src/file-system/file-system.module.ts
+++ backend/src/file-system/file-system.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { FileSystemService } from './file-system.service';
 import { FileSystemController } from './file-system.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FileSystem } from './entities';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([FileSystem])],
   controllers: [FileSystemController],
   providers: [FileSystemService],
+  exports: [TypeOrmModule],
 })
 export class FileSystemModule {}
--- backend/src/windows/windows.module.ts
+++ backend/src/windows/windows.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { WindowsService } from './windows.service';
 import { WindowsController } from './windows.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Window } from './entities/window.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Window])],
   controllers: [WindowsController],
   providers: [WindowsService],
+  exports: [TypeOrmModule],
 })
 export class WindowsModule {}
