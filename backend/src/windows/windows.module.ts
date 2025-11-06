6b6120d0ba184bd704c7105343bc97be97db6863
vaaast-lake <kown9512@gmail.com>
2025-11-06 20:35:42 +0900

    feat: window 모듈에  session, filesystem module import

--- backend/src/windows/windows.module.ts
+++ backend/src/windows/windows.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { WindowsService } from './windows.service';
 import { WindowsController } from './windows.controller';
 import { WindowsRepository } from './windows.repository';
+import { SessionsModule } from 'src/sessions/sessions.module';
+import { FileSystemModule } from 'src/file-system/file-system.module';
 
 @Module({
-  imports: [],
+  imports: [SessionsModule, FileSystemModule],
   controllers: [WindowsController],
   providers: [WindowsService, WindowsRepository],
   exports: [WindowsRepository],
