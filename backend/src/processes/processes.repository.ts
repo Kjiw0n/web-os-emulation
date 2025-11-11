7b3c8b00548b8e91edfd791fc3aeeca67552ab09
vaaast-lake <kown9512@gmail.com>
2025-11-11 21:16:17 +0900

    feat: processes repository 구현

--- backend/src/processes/processes.repository.ts
+++ backend/src/processes/processes.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Repository, DataSource, EntityManager } from 'typeorm';
 import { Process } from './entities';
+import { ProcessStatus } from './types';
 
 @Injectable()
 export class ProcessesRepository {
@@ -13,5 +14,21 @@ export class ProcessesRepository {
     const newRepo = new ProcessesRepository(this.ds);
     newRepo.repo = manager.getRepository(Process);
     return newRepo;
+  }
+
+  async save(process: Process): Promise<Process> {
+    return this.repo.save(process);
+  }
+
+  async findOne(id: number): Promise<Process | null> {
+    return this.repo.findOne({ where: { id } });
+  }
+
+  async findByStatus(status: ProcessStatus): Promise<Process[]> {
+    return this.repo.find({ where: { status } });
+  }
+
+  async findRunningProcesses(): Promise<Process[]> {
+    return this.findByStatus(ProcessStatus.RUNNING);
   }
 }
