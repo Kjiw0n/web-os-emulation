ba8c94c4f007d2e30f02791b41bab0130121a9c8
AYEOOON <k93853471@gmail.com>
2025-11-11 18:30:38 +0900

    feat: SyscallModule에 UnameHandler 추가
    - UnameHandler를 providers에 등록하여 DI 지원
    - SyscallService에서 'uname' 명령어 처리 가능하도록 준비

--- backend/src/syscall/syscall.module.ts
+++ backend/src/syscall/syscall.module.ts
@@ -3,9 +3,10 @@ import { SyscallController } from './syscall.controller';
 import { SyscallService } from './syscall.service';
 import { DateHandler } from './handlers/builtins/date.handler';
 import { HelpHandler } from './handlers/builtins/help.handler';
+import { UnameHandler } from './handlers/builtins/uname.handler';
 
 @Module({
   controllers: [SyscallController],
-  providers: [SyscallService, DateHandler, HelpHandler],
+  providers: [SyscallService, DateHandler, HelpHandler, UnameHandler],
 })
 export class SyscallModule {}
