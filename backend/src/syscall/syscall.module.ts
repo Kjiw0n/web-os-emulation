8e1f6caf1cce652fc1c13ce51098cdde862a54e0
whateveriiwant <seungjun.jeong10@gmail.com>
2025-11-11 21:37:55 +0900

    fix: FileSystemModule 내보내기 수정
    - FileSystemService와 FileSystemRepository를 모두 내보내도록 수정
    - SyscallModule에서 FileSystemModule을 가져오도록 추가

--- backend/src/syscall/syscall.module.ts
+++ backend/src/syscall/syscall.module.ts
@@ -4,8 +4,10 @@ import { SyscallService } from './syscall.service';
 import { DateHandler } from './handlers/builtins/date.handler';
 import { HelpHandler } from './handlers/builtins/help.handler';
 import { LsHandler } from './handlers/file-system/ls.handler';
+import { FileSystemModule } from 'src/file-system/file-system.module';
 
 @Module({
+  imports: [FileSystemModule],
   controllers: [SyscallController],
   providers: [SyscallService, DateHandler, HelpHandler, LsHandler],
 })
