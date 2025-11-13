4f0f504877711091ff2b80bc247057c060ec80d0
vaaast-lake <kown9512@gmail.com>
2025-11-13 17:03:03 +0900

    fix: 환경변수명 변경에 따른 수정

--- backend/src/database/data-source.ts
+++ backend/src/database/data-source.ts
@@ -14,7 +14,7 @@ export const AppDataSource = new DataSource({
   port: configService.get('DB_PORT'),
   username: configService.get('DB_USERNAME'),
   password: configService.get('DB_PASSWORD'),
-  database: configService.get('DB_DATABASE'),
+  database: configService.get('DB_NAME'),
   entities: [FileSystem, Window],
   synchronize: false,
   logging: true,
