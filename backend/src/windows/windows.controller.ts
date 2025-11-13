5e58e1d7daffa9daccc8db874379d94a28d3c7eb
vaaast-lake <kown9512@gmail.com>
2025-11-13 17:50:43 +0900

    fix: window controller에서 사용하지 않는 코드 삭제

--- backend/src/windows/windows.controller.ts
+++ backend/src/windows/windows.controller.ts
@@ -1,18 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { WindowsService } from './windows.service';
-import {
-  CreateWindowDto,
-  CreateWindowResponseDto,
-  UpdateWindowDto,
-} from './dto';
+import { CreateWindowDto, CreateWindowResponseDto } from './dto';
 
 @Controller('windows')
 export class WindowsController {
@@ -21,25 +9,5 @@ export class WindowsController {
   @Post('open')
   openWindow(@Body() dto: CreateWindowDto): Promise<CreateWindowResponseDto> {
     return this.windowsService.create(dto);
-  }
-
-  @Get()
-  findAll() {
-    return this.windowsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.windowsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateWindowDto: UpdateWindowDto) {
-    return this.windowsService.update(+id, updateWindowDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.windowsService.remove(+id);
   }
 }
