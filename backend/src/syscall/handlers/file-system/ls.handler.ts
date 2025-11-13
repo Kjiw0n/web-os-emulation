240c8782defee41a01b92dcfb1a10511c0d49554
whateveriiwant <seungjun.jeong10@gmail.com>
2025-11-13 13:21:18 +0900

    feat: ls 명령어 핸들러 상대경로 로직 구현

--- backend/src/syscall/handlers/file-system/ls.handler.ts
+++ backend/src/syscall/handlers/file-system/ls.handler.ts
@@ -1,13 +1,32 @@
 import { Injectable } from '@nestjs/common';
-import { CommandResult, ICommandHandler } from '../command.interface';
+import {
+  CommandContext,
+  CommandResult,
+  ICommandHandler,
+} from '../command.interface';
 import { FileSystemService } from 'src/file-system/file-system.service';
+import { ProcessesRepository } from 'src/processes/processes.repository';
 
 @Injectable()
 export class LsHandler implements ICommandHandler {
-  constructor(private readonly fileSystemService: FileSystemService) {}
+  constructor(
+    private readonly fileSystemService: FileSystemService,
+    private readonly processesRepo: ProcessesRepository,
+  ) {}
 
-  async execute(args: string[]): Promise<CommandResult> {
-    const path = args[0];
+  async execute(
+    args: string[],
+    _data?: string,
+    context?: CommandContext,
+  ): Promise<CommandResult> {
+    const path = args[0] || '.';
+
+    if (!context?.processId) {
+      return {
+        stdout: '',
+        stderr: 'mkdir: process context required',
+      };
+    }
 
     // 절대 경로 처리
     if (path.startsWith('/')) {
@@ -21,9 +40,47 @@ export class LsHandler implements ICommandHandler {
         if (err instanceof Error) return { stdout: '', stderr: err.message };
         return { stdout: '', stderr: '알 수 없는 오류가 발생했습니다.' };
       }
+    } else {
+      // 상대 경로 처리
+      const process = await this.processesRepo.findOne(context?.processId);
+      if (!process) {
+        return {
+          stdout: '',
+          stderr: 'mkdir: process not found',
+        };
+      }
+
+      try {
+        const currentDirectoryId = process?.currentDirectoryId;
+
+        const relativePathId = await this.fileSystemService.resolveRelativePath(
+          currentDirectoryId,
+          path,
+        );
+
+        const absolutePath =
+          await this.fileSystemService.buildPath(relativePathId);
+
+        const childrenNodes =
+          await this.fileSystemService.listDirectory(absolutePath);
+
+        const output = childrenNodes.map((node) => node.name).join(' ');
+
+        return { stdout: output, stderr: '' };
+      } catch (err) {
+        if (err instanceof Error) {
+          return {
+            stdout: '',
+            stderr: `mkdir: 오류 - ${err.message}`,
+          };
+        }
+        return {
+          stdout: '',
+          stderr: 'mkdir: 알 수 없는 오류가 발생했습니다.',
+        };
+      }
     }
 
-    // TODO: 현재 디렉토리 (CWD) 로직이 추가된 후 상대 경로 및 args가 없을 때 처리 로직 작성
     return { stdout: '', stderr: '현재는 절대 경로만 지원합니다.' };
   }
 }
