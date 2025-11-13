394159f3c666a286d172aef3936ed09a8ef532dd
AYEOOON <k93853471@gmail.com>
2025-11-13 18:58:03 +0900

    feat: FileSystemModule에 ProcessesRepository 등록

--- backend/src/file-system/file-system.module.ts
+++ backend/src/file-system/file-system.module.ts
@@ -3,11 +3,12 @@ import { FileSystemService } from './file-system.service';
 import { FileSystemController } from './file-system.controller';
 import { FileSystemRepository } from './file-system.repository';
 import { S3Module } from 'src/s3/s3.module';
+import { ProcessesRepository } from 'src/processes/processes.repository';
 
 @Module({
   imports: [S3Module],
   controllers: [FileSystemController],
-  providers: [FileSystemService, FileSystemRepository],
+  providers: [FileSystemService, FileSystemRepository, ProcessesRepository],
   exports: [FileSystemService, FileSystemRepository],
 })
 export class FileSystemModule {}
