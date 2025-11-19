0f9503335385d1f061f1e32461234ed87b107059
vaaast-lake <kown9512@gmail.com>
2025-11-19 21:00:38 +0900

    fix: s3 service 코드에 lint 적용

--- backend/src/s3/s3.service.ts
+++ backend/src/s3/s3.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { S3Client, PutObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
+import {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} from '@aws-sdk/client-s3';
 import { ConfigService } from '@nestjs/config';
 import { randomUUID } from 'crypto';
 import { IS3Service } from './s3.interface';
-import { Readable } from "stream";
+import { Readable } from 'stream';
 
 @Injectable()
 export class S3Service implements IS3Service {
@@ -12,16 +16,16 @@ export class S3Service implements IS3Service {
   private region: string;
 
   constructor(private configService: ConfigService) {
-    this.bucketName = this.configService.getOrThrow("NCP_BUCKET_NAME");
-    this.region = this.configService.getOrThrow("NCP_REGION");
+    this.bucketName = this.configService.getOrThrow('NCP_BUCKET_NAME');
+    this.region = this.configService.getOrThrow('NCP_REGION');
 
     this.s3 = new S3Client({
       region: this.region,
-      endpoint: this.configService.getOrThrow("NCP_OBJECT_STORAGE_ENDPOINT"),
+      endpoint: this.configService.getOrThrow('NCP_OBJECT_STORAGE_ENDPOINT'),
       forcePathStyle: true,
       credentials: {
-        accessKeyId: this.configService.getOrThrow("NCP_ACCESS_KEY"),
-        secretAccessKey: this.configService.getOrThrow("NCP_SECRET_KEY"),
+        accessKeyId: this.configService.getOrThrow('NCP_ACCESS_KEY'),
+        secretAccessKey: this.configService.getOrThrow('NCP_SECRET_KEY'),
       },
     });
   }
@@ -34,8 +38,8 @@ export class S3Service implements IS3Service {
         Bucket: this.bucketName,
         Key: key,
         Body: content,
-        ACL: "public-read",
-      })
+        ACL: 'public-read',
+      }),
     );
 
     // NCP Object Storage의 공개 URL 형식
