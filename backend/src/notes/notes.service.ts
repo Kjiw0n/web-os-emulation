ff5144297bb4a1cc0b3aaebc4ec5adaca12c0af4
Kjiw0n <rwd0904@naver.com>
2025-11-21 02:30:12 +0900

    feat: Lobby(: 파일 리스트 업데이트 이벤트 수신 대기 공간) 추가하여 파일 리스트 관리
    - websocket을 통해 mode=list 로 접속한 경우에는 Lobby에 클라이언트 할당
    - notes.service: 로비 브로드캐스트 로직 추가
    - notes.module: provider에 gateway 추가

--- backend/src/notes/notes.service.ts
+++ backend/src/notes/notes.service.ts
@@ -1,10 +1,11 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, forwardRef } from '@nestjs/common';
 import { CreateNotesDto } from './dto/notes.dto';
 import { S3Service } from '../s3/s3.service';
 import { FileSystemRepository } from '../file-system/file-system.repository';
 import { FileSystem } from '../file-system/entities/file-system.entity';
 import { FileType } from '../file-system/types/file-type.enum';
 import { SnapshotService } from './note-snapshot.service';
+import { CollaborationGateway } from 'src/gateway/collaboration.gateway';
 
 export interface CreateNotesResponse {
   fileId: number;
@@ -19,13 +20,15 @@ export class NotesService {
     private readonly fileSystemRepository: FileSystemRepository,
     private readonly snapshotService: SnapshotService,
     @Inject('NOTES_DIR_ID') private readonly notesDirId: number,
+    @Inject(forwardRef(() => CollaborationGateway))
+    private readonly collaborationGateway: CollaborationGateway,
   ) {}
 
   /**
    * 새로운 노트 파일을 생성합니다.
    * @param {CreateNotesDto} createNotesDto - 생성할 노트의 정보 (제목, 내용)
    * @returns {Promise<CreateNotesResponse>} 생성된 파일의 ID, 제목, 내용
-   * @description S3에 파일 내용을 저장하고, 파일 시스템에 메타데이터를 저장합니다.
+   * @description S3에 파일 내용을 저장하고, 파일 시스템에 메타데이터를 저장합니다. Lobby에 파일 생성 알림을 전송합니다.
    */
   async create(createNotesDto: CreateNotesDto): Promise<CreateNotesResponse> {
     const fileName = createNotesDto.name;
@@ -46,6 +49,13 @@ export class NotesService {
     fileSystem.permissions = 'rwx';
 
     const savedFileSystem = await this.fileSystemRepository.save(fileSystem);
+
+    // 로비에 "새 파일 생성됨" 알림 전송
+    this.collaborationGateway.broadcastToLobby('create', {
+      id: savedFileSystem.id,
+      name: savedFileSystem.name,
+      updatedAt: savedFileSystem.updatedAt,
+    });
 
     return {
       fileId: savedFileSystem.id,
